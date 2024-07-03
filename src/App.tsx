beb907492021183de6a9045cabe93e47242463ae
Nikolay Anikin <nick.anikin.2000@gmail.com>
2024-07-03 15:38:35 +0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          CI/CD Workshop
+          CI/CD Workshop fix
         </p>
         <a
           className="App-link"
